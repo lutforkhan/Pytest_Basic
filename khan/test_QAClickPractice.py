a66588fe39e007b10e35f523854ebb64f7bec035
Muhammad Khan <lfk485@yahoo.com>
2020-11-06 22:40:55 -0500

    allure tags

--- khan/test_QAClickPractice.py
+++ khan/test_QAClickPractice.py
@@ -1,19 +1,31 @@
-
+import allure
+from behave import Step
 from selenium import webdriver
 from selenium.webdriver import firefox
 #from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import chrome
+from selenium.webdriver.support.ui import Select
 from time import sleep
 import pytest
 class Test_Class:
-
+    # @Epic
+    # @Features
+    # @Stories / @ Story
+    # @Severity(SeverityLevel.BLOCKER)
+    # @Description("In this cool test we will check cool thing")
+    # @allure.step
+    # @Step("this is the first step tetst khan")
+    # # @Attachment
+    # @Link
+    @allure.description("First test descption in allure framework")
     def test_khan1(self):
         print("Hi Khan")
 
-
+    @allure.step("this is the first step of Fatema test")
     def test_fatema(self):
         print("Hi Fatema")
     @pytest.fixture
+    @Step("this is the step of fixture")
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path=".\\drivers\\chromedriver.exe")
         self.driver.get("https://rahulshettyacademy.com/AutomationPractice/")
@@ -23,6 +35,8 @@ class Test_Class:
         self.driver.close()
 
     @pytest.mark.sanity
+    @allure.step("this is the sanity testing")
+    @allure.severity(severity_level="Hotfix")
     def test_selectRadioButton(self,setUp):
         self.driver.find_element_by_xpath("//*[@id=\"radio-btn-example\"]/fieldset/label[1]/input").click()
         sleep(1)
@@ -31,6 +45,9 @@ class Test_Class:
         self.driver.find_element_by_xpath("//*[@id=\"radio-btn-example\"]/fieldset/label[3]/input").click()
         sleep(1)
 
+    @pytest.mark.regression
+    @allure.link("www.google.com")
+    @allure.testcase("www.google.com","TestCase_001")
     def test_selectOptionButton(self, setUp):
         self.driver.find_element_by_xpath("//*[@id=\"checkBoxOption1\"]").click()
         sleep(1)
@@ -39,23 +56,15 @@ class Test_Class:
         self.driver.find_element_by_xpath("//*[@id=\"checkBoxOption3\"]").click()
         sleep(1)
 
-        # // Select
-        # List
-        # By
-        # selectList = By.id("dropdown-class-example");
-        # // Select
-        # option
-        # buttons
-        # By
-        # optionButton1 = By.xpath("");
-        # By
-        # optionButton2 = By.xpath("//*[@id=\"checkBoxOption2\"]");
-        # By
-        # optionButton3 = By.xpath("//*[@id=\"checkBoxOption3\"]");
-        #
-        #
-
-
+    @pytest.mark.regression
+    def test_selectDropDownList(self, setUp):
+        ele=self.driver.find_element_by_xpath("//*[@id='dropdown-class-example']")
+        obj=Select(ele)
+        obj.select_by_index(0)
+        sleep(1)
+        obj.select_by_index(1)
+        sleep(1)
+        
 
 
 
--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
-
-C:\Users\khan\PycharmProjects\GitHubRepo\Pytest_Basic>pytest -s -v
+rem is for inactive the command, I have created this bat file in project folder
+rem pytest -s -v
+rem pytest -s -v -m "sanity"
+pytest -s -v -m "sanity" --html=./Reports/report.html
+rem -k is used to execute with partial text
+pytest -s -v -k "sani" --html=./Reports/report.html khan\
